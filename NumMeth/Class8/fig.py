1e514361e950896cbea250c370b86c3be46de247
watabegg <roy_wtnb@outlook.jp>
2023-07-26 01:37:01 +0900

    update
    NumMeth - 8

--- NumMeth/Class8/fig.py
+++ NumMeth/Class8/fig.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=np.arange(0.0, 10.0, 0.005)
+x=np.arange(0.01, 10.0, 0.005)
 
 dx=[0.01,0.1,0.5,1,2,4,8,10]
 dy=[218,26,8,4,1,8,31,230]
@@ -12,5 +12,5 @@ plt.legend([r'$y=\frac{2.185093}{x}+0.010431e^x$'])
 plt.scatter(dx,dy,c="red")
 plt.xlabel('x')
 plt.ylabel('y')
-plt.savefig("./LaTeX/NumMeth/8/plot_2.png")
+plt.savefig("./LaTeX/NumMeth/8/plot.png")
 plt.show()
