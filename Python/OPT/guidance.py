a59ba80660a746a1c0eeabc26f0285d83239d630
watabegg <roy_wtnb@outlook.jp>
2023-09-26 09:55:50 +0900

    introduct Optimization

---
+++ Python/OPT/guidance.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def f_x(x):
+    return x**2 + x - 1
+
+xvalues = range (1,11)
+
+yvalues = [f_x(x) for x in xvalues]
+
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title(r"Plot of $x^2 + x -1$ ")
+plt.plot(xvalues,yvalues)
+plt.savefig("guidance.png")
