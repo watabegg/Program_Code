29673acd27253d69f29b4d1c4b1be7f5b66fca13
watabegg <roy_wtnb@outlook.jp>
2023-11-01 16:19:09 +0900

    update CSE2-Class2

---
+++ Python/CSE2/Bernoulli.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+LOOP = 10 ** 6
+P = 0.1
+counter = []
+
+for i in range(LOOP):
+    count = 1
+    while random.random() > P:
+        count += 1
+    counter.append(count)
+
+hlist = np.histogram(counter, range=(1,50), bins=50, density=False)
+print(hlist) #各到着間隔の個数リスト
+plt.figure()
+plt.hist(counter, range=(1, 50), bins=50) #到着間隔のヒストグラム
+print(hlist[0]/LOOP) #hlistを相対度数(確率)に計算したやつ
+plt.figure()
+plt.bar(np.arange(0,50), hlist[0]/LOOP, width=1.0) #相対度数のヒストグラム
+plt.show()
