733e5bc4eaf0e2688fde0ec2f051e165f0d1cb6e
watabegg <roy_wtnb@outlook.jp>
2025-06-19 23:18:59 +0900

    あ

--- Python/BuiltIn/9/exam9-1.py
+++ Python/BuiltIn/9/exam9-1.py
@@ -24,34 +24,25 @@ def mcp3002(ch):
     return ad
 
 def read_adt7410():
-    """ADT7410から温度を読み取る関数"""
     try:
-        # 上位バイトと下位バイトを読み込み 
         word_data = i2c.read_word_data(adt7410_address, 0x00)
-        # バイトオーダーをスワップ
         data = (word_data & 0xFF) << 8 | (word_data >> 8)
-        # 13bitモードなので3bit右シフト 
         data = data >> 3
         
-        # 2の補数表現で負の値を判定
-        if data & 0x1000: # 13bit目で判定
-            data -= 8192 # 2^13 = 8192
+        if data & 0x1000:
+            data -= 8192
             
-        # 温度に換算 (分解能 0.0625℃) 
         temperature = data * 0.0625
         return temperature
     except IOError:
         return None
 
 def read_mcp9700a():
-    """MCP9700Aの値をMCP3002経由で読み取り温度に変換する関数"""
     try:
         data = mcp3002(ch0)
         
-        # デジタル値を電圧に変換 (10bit, 3.3V)
         voltage = (data * 3.3) / 1023.0
         
-        # 電圧を温度に変換 (℃) 
         temperature = (voltage / 0.01)
         return temperature
     except Exception as e:
@@ -72,18 +63,3 @@ except KeyboardInterrupt:
 finally:
     spi.close()
     i2c.close()
-    
-# 2025/06/19 18:20
-# ADT7410 (Digital): 27.56 C, MCP9700A (Analog): 27.42 C
-# ADT7410 (Digital): 27.50 C, MCP9700A (Analog): 27.10 C
-# ADT7410 (Digital): 28.12 C, MCP9700A (Analog): 27.10 C
-# ADT7410 (Digital): 27.94 C, MCP9700A (Analog): 27.42 C
-# ADT7410 (Digital): 29.00 C, MCP9700A (Analog): 27.42 C
-
-# 2025/06/19 19:20
-# ADT7410 (Digital): 27.56 C, MCP9700A (Analog): 26.45 C
-# ADT7410 (Digital): 26.94 C, MCP9700A (Analog): 26.45 C
-# ADT7410 (Digital): 27.50 C, MCP9700A (Analog): 26.45 C
-# ADT7410 (Digital): 27.50 C, MCP9700A (Analog): 26.45 C
-# ADT7410 (Digital): 27.06 C, MCP9700A (Analog): 26.13 C
-# ADT7410 (Digital): 26.75 C, MCP9700A (Analog): 26.45 C
--- Python/BuiltIn/9/exam9-2.py
+++ Python/BuiltIn/9/exam9-2.py
@@ -1,37 +1,30 @@
 import spidev
 import time
 
-# --- SPIデバイスの準備 ---
-# デバイス0 (CE0) for ADC (MCP3002)
 adc_spi = spidev.SpiDev()
-adc_spi.open(0, 0) # SPIバス0, デバイス0 (CE0)
+adc_spi.open(0, 0)
 adc_spi.max_speed_hz = 10000
 
-# デバイス1 (CE1) for Accelerometer (LIS3DH)
 lis_spi = spidev.SpiDev()
-lis_spi.open(0, 1) # SPIバス0, デバイス1 (CE1) 
+lis_spi.open(0, 1)
 lis_spi.max_speed_hz = 10000
 
-# --- MCP3002 制御用定数 ---
 start = 0b01000000
 sgl   = 0b00100000
 ch0   = 0b00000000
 msbf  = 0b00001000
 
-# --- LIS3DH レジスタアドレス等 ---
 CTRL_REG1 = 0x20
 OUT_X_L = 0x28
 OUT_Y_L = 0x2A
 OUT_Z_L = 0x2C
 
 def mcp3002(ch):
-    """MCP3002から指定チャンネルの値を読み取る (ADC用SPIを使用)"""
     rcv = adc_spi.xfer2([(start + sgl + ch + msbf), 0x00])
     ad = (((rcv[0] & 0x03) << 8) + rcv[1])
     return ad
 
 def read_mcp9700a():
-    """MCP9700Aの値をMCP3002経由で読み取り温度に変換する関数"""
     try:
         data = mcp3002(ch0)
         voltage = (data * 3.3) / 1023.0
@@ -43,29 +36,20 @@ def read_mcp9700a():
         return None
 
 def setup_lis3dh():
-    """LIS3DHを初期化する (加速度センサ用SPIを使用)"""
-    # CTRL_REG1 (0x20) に 0x27 を書き込み 
-    # ODR=10Hz, Normal mode, X/Y/Z軸有効 
     lis_spi.xfer2([CTRL_REG1, 0x27])
 
 def read_lis3dh_axis_g(reg_l):
-    """LIS3DHの指定された軸のデータを読み取り、g値に変換して返す"""
     reg_h = reg_l + 1
     low_byte = lis_spi.xfer2([reg_l | 0x80, 0x00])[1]
     high_byte = lis_spi.xfer2([reg_h | 0x80, 0x00])[1]
     
-    # 2バイトを結合して16bitのデータにする
     value = (high_byte << 8) | low_byte
     
-    # 2の補数表現で負の値を判定
     if value > 32767:
         value -= 65536
     
-    # 12bitデータなので4bit右シフトしてraw値を取得
     raw_value = value >> 4
     
-    # raw値をg値に変換 (±2gモードの場合)
-    # 12bitの最大値(2047)が+2gに相当
     g_value = (raw_value / 2047.0) * 2.0
     return g_value
 
@@ -93,10 +77,3 @@ finally:
     adc_spi.close()
     lis_spi.close()
     
-# Temp: 27.42 C | Accel X: 0.007816316560820713, Y: 0.03517342452369321, Z: 1.0591108939912066
-# Temp: 27.42 C | Accel X: 0.007816316560820713, Y: 0.027357107962872496, Z: 1.0669272105520273
-# Temp: 26.45 C | Accel X: -0.7816316560820713, Y: 0.12115290669272105, Z: 0.680019540791402
-# Temp: 26.77 C | Accel X: -0.9965803615046409, Y: 0.05471421592574499, Z: 0.1250610649731314
-# Temp: 26.77 C | Accel X: -0.050806057645334635, Y: 1.0473864191499755, Z: 0.05862237420615535
-# Temp: 26.45 C | Accel X: -0.0039081582804103565, Y: 1.051294577430386, Z: -0.03126526624328285
-# Temp: 27.10 C | Accel X: 0.0039081582804103565, Y: 0.039081582804103565, Z: 1.063019052271617
